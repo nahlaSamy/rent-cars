69cd878771851fade1735a5c387d11c3969c88f3
Nahla samy <Nahlasamy583@gmail.com>
2024-05-23 22:51:34 +0300

    removing unnecessary code

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "bootstrap/dist/js/bootstrap.bundle.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@fortawesome/fontawesome-free/js/all.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
